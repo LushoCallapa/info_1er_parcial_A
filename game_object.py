0bd1d918e980e4c3823f3e11a378e7ab9cbf65bd
LushoCallapa <luiscallapa2004@gmail.com>
2024-08-24 20:59:07 -0400

    Examen Primer Parcial

--- game_object.py
+++ game_object.py
@@ -39,7 +39,7 @@ class Bird(arcade.Sprite):
         shape.elasticity = elasticity
         shape.friction = friction
         shape.collision_type = collision_layer
-
+        self.has_boosted = True
         space.add(body, shape)
 
         self.body = body
@@ -52,6 +52,109 @@ class Bird(arcade.Sprite):
         self.center_x = self.shape.body.position.x
         self.center_y = self.shape.body.position.y
         self.radians = self.shape.body.angle
+
+    def on_click(self):
+        if not self.has_boosted:
+            self.has_boosted = True
+
+class YellowBird(Bird):
+    def __init__(self, 
+        image_path: str, 
+        impulse_vector: ImpulseVector, 
+        x: float, 
+        y: float, 
+        space: pymunk.Space, 
+        mass: float = 5, 
+        radius: float = 12, 
+        max_impulse: float = 100, 
+        power_multiplier: float = 50, 
+        elasticity: float = 0.8,
+        friction: float = 1, 
+        collision_layer: int = 0, 
+        impulse: float = 2.0
+    ):
+        super().__init__(image_path, 
+            impulse_vector, 
+            x, 
+            y, 
+            space, 
+            mass, 
+            radius, 
+            max_impulse, 
+            power_multiplier, 
+            elasticity, 
+            friction, 
+            collision_layer)
+        self.impulse = impulse
+        self.has_boosted = False
+
+    def on_click(self):
+        if not self.has_boosted:
+            #ya que no son 2 puntos solo cambiar el impulso no utilize las funciones del game logic
+            impulse_final = self.impulse * self.body.velocity.length
+            impulse_vector = pymunk.Vec2d(impulse_final, 0).rotated(self.body.angle)
+            self.body.apply_impulse_at_local_point(impulse_vector)
+            self.has_boosted = True
+
+class BlueBird(Bird):
+    def __init__(self, 
+        image_path: str, 
+        impulse_vector: ImpulseVector, 
+        x: float, y: float, 
+        space: pymunk.Space, 
+        sprites_list: arcade.SpriteList,
+        birds_list: arcade.SpriteList,
+        mass: float = 5, 
+        radius: float = 12, 
+        max_impulse: float = 100, 
+        power_multiplier: float = 50, 
+        elasticity: float = 0.8, 
+        friction: float = 1, 
+        collision_layer: int = 0, 
+        ):
+        super().__init__(image_path, 
+        impulse_vector, 
+        x, 
+        y, 
+        space, 
+        mass, 
+        radius, 
+        max_impulse, 
+        power_multiplier, 
+        elasticity, 
+        friction, 
+        collision_layer,
+        )
+
+        self.sprites_list = sprites_list  
+        self.birds_list = birds_list
+        self.bird_split = False
+
+    def on_click(self):
+        if not self.bird_split:
+            new_angles = [self.body.angle + math.radians(20),
+                      self.body.angle- math.radians(10),
+                      self.body.angle - math.radians(40)]
+
+            for angle in new_angles:
+                velocity = self.body.velocity.rotated(angle - self.body.angle)
+                new_bird = Bird(
+                    self.texture.name,  
+                    ImpulseVector(velocity.length, angle),
+                    self.body.position.x,
+                    self.body.position.y,
+                    self.shape.space,
+                    max_impulse=velocity.length,
+                )
+
+                new_bird.body.velocity = velocity
+
+                self.sprites_list.append(new_bird)
+                self.birds_list.append(new_bird)
+
+            self.remove_from_sprite_lists()
+            self.shape.space.remove(self.shape, self.body)
+            self.bird_split = True  
 
 
 class Pig(arcade.Sprite):
@@ -121,6 +224,9 @@ class Column(PassiveObject):
     def __init__(self, x, y, space):
         super().__init__("assets/img/column.png", x, y, space)
 
+class Beam(PassiveObject):
+    def __init__(self, x, y, space):
+        super().__init__("assets/img/beam.png", x, y, space)
 
 class StaticObject(arcade.Sprite):
     def __init__(
--- main.py
+++ main.py
@@ -2,8 +2,9 @@ import math
 import logging
 import arcade
 import pymunk
+from game_logic import get_angle_radians
 
-from game_object import Bird, Column, Pig
+from game_object import Bird, BlueBird, Column, Pig, YellowBird, Beam
 from game_logic import get_impulse_vector, Point2D, get_distance
 
 logging.basicConfig(level=logging.DEBUG)
@@ -23,7 +24,12 @@ class App(arcade.Window):
     def __init__(self):
         super().__init__(WIDTH, HEIGHT, TITLE)
         self.background = arcade.load_texture("assets/img/background3.png")
+        self.slingshot_texture = arcade.load_texture("assets/img/sling-3.png")
+        self.current_bird_image = "assets/img/red-bird3.png"
+        self.beam_texture = arcade.load_texture("assets/img/beam.png")
         # crear espacio de pymunk
+
+        
         self.space = pymunk.Space()
         self.space.gravity = (0, GRAVITY)
 
@@ -36,17 +42,33 @@ class App(arcade.Window):
         self.sprites = arcade.SpriteList()
         self.birds = arcade.SpriteList()
         self.world = arcade.SpriteList()
-        self.add_columns()
+        #self.add_columns()
+        self.add_piramide()
         self.add_pigs()
 
         self.start_point = Point2D()
         self.end_point = Point2D()
         self.distance = 0
         self.draw_line = False
+        self.draw_slingshot = False
 
-        # agregar un collision handler
+        # agregar un collision handlers
         self.handler = self.space.add_default_collision_handler()
         self.handler.post_solve = self.collision_handler
+        self.actual_bird = "Red"
+
+
+    
+    def on_key_press(self, key, modifiers):
+        if key == arcade.key.Z:
+            self.current_bird_image = "assets/img/red-bird3.png"
+            self.actual_bird = "Red"
+        elif key == arcade.key.X:
+            self.current_bird_image = "assets/img/blue.png"
+            self.actual_bird = "Blue"
+        elif key == arcade.key.C:
+            self.current_bird_image = "assets/img/yellow.png"
+            self.actual_bird = "Yellow"
 
     def collision_handler(self, arbiter, space, data):
         impulse_norm = arbiter.total_impulse.length
@@ -61,6 +83,45 @@ class App(arcade.Window):
 
         return True
 
+    def add_piramide(self):
+        column = Column(WIDTH // 2 + 40, 150, self.space)
+        self.sprites.append(column)
+        self.world.append(column)
+        column = Column(WIDTH // 2 + 117, 150, self.space)
+        self.sprites.append(column)
+        self.world.append(column)
+        column = Beam(WIDTH // 2 + 79, 200, self.space)
+        self.sprites.append(column)
+        self.world.append(column)
+        column = Column(WIDTH // 2, 50, self.space)
+        self.sprites.append(column)
+        self.world.append(column)
+        column = Column(WIDTH // 2 + 77, 50, self.space)
+        self.sprites.append(column)
+        self.world.append(column)
+        column = Column(WIDTH // 2 + 154, 50, self.space)
+        self.sprites.append(column)
+        self.world.append(column)
+        column = Beam(WIDTH // 2 + 35, 100, self.space)
+        self.sprites.append(column)
+        self.world.append(column)
+        column = Beam(WIDTH // 2 + 120, 100, self.space)
+        self.sprites.append(column)
+        self.world.append(column)
+
+
+        column = Column(WIDTH // 2 + 250, 50, self.space)
+        self.sprites.append(column)
+        self.world.append(column)
+        column = Column(WIDTH // 2 + 327, 50, self.space)
+        self.sprites.append(column)
+        self.world.append(column)
+        column = Beam(WIDTH // 2 + 285, 100, self.space)
+        self.sprites.append(column)
+        self.world.append(column)
+
+        
+
     def add_columns(self):
         for x in range(WIDTH // 2, WIDTH, 400):
             column = Column(x, 50, self.space)
@@ -68,9 +129,20 @@ class App(arcade.Window):
             self.world.append(column)
 
     def add_pigs(self):
-        pig1 = Pig(WIDTH / 2, 100, self.space)
+        pig1 = Pig(WIDTH / 2 + 50 , 50, self.space)
         self.sprites.append(pig1)
         self.world.append(pig1)
+        pig2 = Pig(WIDTH / 2 + 125 , 50, self.space)
+        self.sprites.append(pig2)
+        self.world.append(pig2)
+
+        pig2 = Pig(WIDTH / 2 + 90 , 150, self.space)
+        self.sprites.append(pig2)
+        self.world.append(pig2)
+
+        pig2 = Pig(WIDTH / 2 + 300 , 50, self.space)
+        self.sprites.append(pig2)
+        self.world.append(pig2)
 
     def on_update(self, delta_time: float):
         self.space.step(1 / 60.0)  # actualiza la simulacion de las fisicas
@@ -85,19 +157,48 @@ class App(arcade.Window):
             self.start_point = Point2D(x, y)
             self.end_point = Point2D(x, y)
             self.draw_line = True
+            self.draw_slingshot = True
             logger.debug(f"Start Point: {self.start_point}")
+
+        elif button == arcade.MOUSE_BUTTON_RIGHT:
+            for bird in self.birds:
+                bird.on_click()
 
     def on_mouse_drag(self, x: int, y: int, dx: int, dy: int, buttons: int, modifiers: int):
         if buttons == arcade.MOUSE_BUTTON_LEFT:
-            self.end_point = Point2D(x, y)
-            logger.debug(f"Dragging to: {self.end_point}")
+            temp_end_point = Point2D(x, y)
+            distance = get_distance(self.start_point, temp_end_point)
+            
+            if distance > 100:
+                angle = get_angle_radians(self.start_point, temp_end_point)
+                x = self.start_point.x - 100 * math.cos(angle)
+                y = self.start_point.y - 100 * math.sin(angle)
+        
+        self.end_point = Point2D(x, y)
+        logger.debug(f"Dragging to: {self.end_point}")
 
     def on_mouse_release(self, x: int, y: int, button: int, modifiers: int):
         if button == arcade.MOUSE_BUTTON_LEFT:
             logger.debug(f"Releasing from: {self.end_point}")
             self.draw_line = False
+            self.draw_slingshot = False
+            
+            distance = get_distance(self.start_point, self.end_point)
+            
+            if distance > 100:
+                angle = get_angle_radians(self.start_point, self.end_point)
+                x = self.start_point.x - 100 * math.cos(angle)
+                y = self.start_point.y - 100 * math.sin(angle)
+                self.end_point = Point2D(x, y)
+            
             impulse_vector = get_impulse_vector(self.start_point, self.end_point)
-            bird = Bird("assets/img/red-bird3.png", impulse_vector, x, y, self.space)
+
+            if self.actual_bird == "Yellow":
+                bird = YellowBird(self.current_bird_image, impulse_vector, self.end_point.x, self.end_point.y, self.space)
+            elif self.actual_bird == "Blue":
+                bird = BlueBird(self.current_bird_image, impulse_vector, self.end_point.x, self.end_point.y, self.space, self.sprites, self.birds)
+            else:
+                bird = Bird(self.current_bird_image, impulse_vector, self.end_point.x, self.end_point.y, self.space)
             self.sprites.append(bird)
             self.birds.append(bird)
 
@@ -108,6 +209,47 @@ class App(arcade.Window):
         if self.draw_line:
             arcade.draw_line(self.start_point.x, self.start_point.y, self.end_point.x, self.end_point.y,
                              arcade.color.BLACK, 3)
+            
+        if  self.draw_slingshot:
+            arcade.draw_lrwh_rectangle_textured(self.start_point.x - 25, self.start_point.y - 25, 50, 50, self.slingshot_texture)
+        launch_icon = arcade.load_texture("assets/img/click_left.png")
+        power_icon = arcade.load_texture("assets/img/click_right.png")
+        
+
+        icon_width = 45
+        icon_height = 45
+        x_position = 10
+        y_position_launch = HEIGHT - 60
+        y_position_power = y_position_launch - icon_height - 10
+        
+        arcade.draw_lrwh_rectangle_textured(x_position, y_position_launch, icon_width, icon_height, launch_icon)
+        arcade.draw_text("Lanzar Pájaro", x_position + icon_width + 10, y_position_launch + 15, arcade.color.BLACK, 14)
+        
+        arcade.draw_lrwh_rectangle_textured(x_position, y_position_power, icon_width, icon_height, power_icon)
+        arcade.draw_text("Activar Power", x_position + icon_width + 10, y_position_power + 15, arcade.color.BLACK, 14)
+
+        bird_icons = [
+            ("assets/img/red-bird3.png", "Z", "Red"),
+            ("assets/img/blue.png", "X", "Blue"),
+            ("assets/img/yellow.png", "C", "Yellow")
+        ]
+
+        icon_width = 45
+        icon_height = 45
+        x_position = WIDTH - (icon_width + 450)
+        
+        for index, (icon_path, key, name) in enumerate(bird_icons):
+            icon_texture = arcade.load_texture(icon_path)
+            y_position = HEIGHT - (60 + index * (icon_height + 20))
+            
+            arcade.draw_lrwh_rectangle_textured(x_position, y_position, icon_width, icon_height, icon_texture)
+
+            arcade.draw_text(f"Tecla: {key}", x_position + icon_width + 10, y_position + 15, arcade.color.BLACK, 14)
+            arcade.draw_text(f"Pájaro: {name}", x_position + icon_width + 10, y_position - 5, arcade.color.BLACK, 14)
+        
+        
+
+    
 
 
 def main():
